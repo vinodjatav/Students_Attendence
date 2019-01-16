b2ebf1b036d6bbe58137d70748be65df4233d0fb
Vinod Jatav <33261019+vinodjatav@users.noreply.github.com>
2019-01-17 01:08:38 +0530

    Add files via upload

---
+++ main/java/com/example/vj/students_attendence/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.vj.students_attendence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btntake,btnadd,btndelete,btnhelp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* btntake.setOnClickListener(findViewById(R.id.btntake));
+        btnadd.setOnClickListener(findViewById(R.id.btnadd));
+        btndelete.setOnClickListener(findViewById(R.id.btndelete));
+        btnhelp.setOnClickListener((View.OnClickListener) findViewById(R.id.btnhelp));*/
+
+
+    }
+}
